6457fe233e0f153569dd6b8df454ea21dbd5ec97
Michael Bahr <michael@bahr.dev>
2022-12-27 17:55:30 +0100

    chore: simplify command

--- src/functions/registerDiscordCommands/handler.ts
+++ src/functions/registerDiscordCommands/handler.ts
@@ -31,34 +31,6 @@ async function writeCommands() {
     "name": "blep",
     "type": 1,
     "description": "Send a random adorable animal photo",
-    "options": [
-      {
-        "name": "animal",
-        "description": "The type of animal",
-        "type": 3,
-        "required": true,
-        "choices": [
-          {
-            "name": "Dog",
-            "value": "animal_dog"
-          },
-          {
-            "name": "Cat",
-            "value": "animal_cat"
-          },
-          {
-            "name": "Penguin",
-            "value": "animal_penguin"
-          }
-        ]
-      },
-      {
-        "name": "only_smol",
-        "description": "Whether to show only baby animals",
-        "type": 5,
-        "required": false
-      }
-    ]
   }]
 
   await client.put(`/applications/${APPLICATION_ID}/commands`, commands);
