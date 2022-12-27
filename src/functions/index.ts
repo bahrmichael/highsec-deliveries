95b4ecf1c24c91f94729f7e5f4a9a5516a40c123
Michael Bahr <michael@bahr.dev>
2022-12-27 17:41:04 +0100

    chore: deploy function to register discord commands

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,2 +1,3 @@
 export { default as hello } from './hello';
 export { default as interactions } from './interactions';
+export { default as registerDiscordCommands } from './registerDiscordCommands';
