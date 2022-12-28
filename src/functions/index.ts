a8ae6a657bf792823a02bba7b6ea2a300c3cf3e7
Michael Bahr <michael@bahr.dev>
2022-12-28 12:34:17 +0100

    fix: deploy callback function

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,3 +1,4 @@
 export { default as hello } from './hello';
 export { default as interactions } from './interactions';
 export { default as registerDiscordCommands } from './registerDiscordCommands';
+export { default as ssoCallback } from './sso-callback';
