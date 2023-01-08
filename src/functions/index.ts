e5ad96877a30e1a94e87537cd79fbcf537e9ff45
Michael Bahr <michael@bahr.dev>
2023-01-08 19:41:19 +0100

    chore: add function parse contracts

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -3,4 +3,5 @@ export { default as interactions } from './interactions';
 export { default as registerDiscordCommands } from './registerDiscordCommands';
 export { default as ssoCallback } from './sso-callback';
 export { default as parseCorpWallet } from './parseCorpWallet';
+export { default as parseContracts } from './parseContracts';
 export { default as updateBalance } from './updateBalance';
